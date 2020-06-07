d56a4dd51ea99cea3ba7adb2d49bb5262d91f0e0
Mateus Pereira <mateusdeap@gmail.com>
2020-06-07 15:24:34 -0300

    Refactors methods to use I18n translations and abstracted a fetch method responsible for randomly retrieveing data from the pt.yml file

--- lib/detran_faker/habilitacao/cnh.rb
+++ lib/detran_faker/habilitacao/cnh.rb
@@ -4,59 +4,58 @@ module DetranFaker
   # Module that contains any classes to generate data
   # related to licensing issues
   module Habilitacao
-    @state_initials = %w[AM AC AL AP AM BA CE DF
-                         ES GO MA MT MS MG PA PB PR PE PI
-                         RJ RN RS RO RR SC SP SE TO]
+    # Base class for the Habilitacao module
+    class Base
+      class << self
+        def random_single_digit
+          Random.rand(0..9).to_s
+        end
 
-    class << self
-      def random_single_digit
-        Random.rand(0..9).to_s
-      end
+        def random_non_zero_digit
+          Random.rand(1..9).to_s
+        end
 
-      def random_non_zero_digit
-        Random.rand(1..9).to_s
-      end
-
-      def random_state_initials
-        @state_initials[Random.rand(0..(@state_initials.size - 1))]
+        def fetch(key:)
+          fetched_values = I18n.translate("detran_faker.#{key}")
+          fetched_values[Random.rand(0..(fetched_values.size - 1))]
+        end
       end
     end
 
-    # Module responsible for generating fake information
+    # Class responsible for generating fake information
     # that simulates a national driver's license.
-    class CNH
+    class CNH < Base
       class << self
         def numero_registro_nacional
-          cnh_number = Habilitacao.random_non_zero_digit
+          cnh_number = random_non_zero_digit
           10.times do
-            cnh_number = cnh_number.dup.concat Habilitacao.random_single_digit
+            cnh_number = cnh_number.dup.concat random_single_digit
           end
 
           cnh_number
         end
 
         def numero_espelho
-          numero_espelho = Habilitacao.random_non_zero_digit
+          numero_espelho = random_non_zero_digit
           9.times do
             numero_espelho =
-              numero_espelho.dup.concat Habilitacao.random_single_digit
+              numero_espelho.dup.concat random_single_digit
           end
 
           numero_espelho
         end
 
         def numero_formulario_renach
-          renach = Habilitacao.random_state_initials
+          renach = fetch(key: "habilitacao.abreviacoes_estados")
           9.times do
-            renach = renach.dup.concat Habilitacao.random_single_digit
+            renach = renach.dup.concat random_single_digit
           end
 
           renach
         end
 
         def categoria
-          possible_categories = %w[A B C D E ACC]
-          possible_categories[Random.rand(0..(possible_categories.size - 1))]
+          fetch(key: "habilitacao.cnh.categorias")
         end
       end
     end
